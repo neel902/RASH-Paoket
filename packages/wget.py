fc8ca23de91ef84853119a6807a24b84516c6c99
neel902 <83077621+neel902@users.noreply.github.com>
2026-09-05 11:17:02 +0100

    Add user input for URL and destination path
    Prompt user for URL and destination path before downloading.

--- packages/wget.py
+++ packages/wget.py
@@ -1,12 +1,13 @@
 import urllib.request
 
-url = input()
+url = input("URL: ")
+destination = input("Destination path: ")
 
 try:
   with urllib.request.urlopen(url) as response:
       # Read and decode bytes to text string
       content = response.read().decode('utf-8')
+  RashFS.write(destination, content)
 except Exception as e:
   print("Error: " + str(e))
 
-print(content)
